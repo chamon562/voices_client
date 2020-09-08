862c538dec0fa7646d52721c014b0f7d7c7ec49b
Philip Yap <philipyap@Philips-MacBook.local>
2020-09-07 23:51:38 -0700

    [user]edited profile page

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,37 +1,42 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import {Link} from 'react-router-dom'
 
-const Profile = (props) => {
-    console.log(props)
-  const userDate = props.user ? (
-    <div>
-      <h1>Profile</h1>
-      <p>
-        {" "}
-        <strong>Name:</strong> {props.user.name}
-      </p>
-      <p>
-        <strong>Email:</strong> {props.user.email}
-      </p>
-      <p>
-        <strong>ID:</strong>
-        {props.user.id}
-      </p>
-    </div>
-  ) : (
-    <h4>Loading...</h4>
-  );
+const Profile = (props) =>{
+   
 
-  const errorDiv = () => {
-    return (
-      <div className="text-center pt-4">
-        <h3>
-          Please <Link to="/login">Login</Link> to view this page{" "}
-        </h3>
-      </div>
-    );
-  };
-  return <div>{props.user ? userDate : errorDiv()}</div>;
-};
+    // handleDelete = () => {
+    //     axios.delete(`${REACT_APP_SERVER_URL}/api/users/:id`, userData)
+    //     .then()
+    // }
 
-export default Profile;
+
+    const userData = props.user ?
+    
+    (<div>
+        <h1>Profile</h1>
+        <p><strong>Name: </strong> {props.user.name}</p>
+        <p><strong>Email: </strong> {props.user.email}</p>
+        <p><strong>ID: </strong>{props.user.id}</p>
+        <p>content: {props.user.content}</p>
+        <p>birthday: {props.user.birthday}</p>
+        <p>artist type: {props.user.artistType}</p>
+        <p>bio : {props.user.bio}</p>
+    </div>) : <h4>Loading ...</h4>
+
+        const errorDiv = () => {
+            return (
+                <div className="text-center pt-4">
+                    <h3>Please <Link to="/login">login</Link> to view this page</h3>
+                </div>
+            )
+        }
+
+        return(
+            <div>
+                {props.user ? userData: errorDiv()}
+                
+            </div>
+        )
+        console.log(props.user)
+}
+export default Profile
